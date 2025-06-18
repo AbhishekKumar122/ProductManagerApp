f6c29ed8a446a4a7b2b4eede830f8efbbbe3a493
AbhishekKumar122 <abhihariyale22@gmail.com>
2025-06-18 19:52:59 +0530

    fixes issues

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -41,6 +41,7 @@ const Home = () => {
           </button>
           
         </div>
+        
         <AddProduct onAdd={addProduct} />
         <div className="my-4">
           <SearchBar onSearch={setQuery} />
